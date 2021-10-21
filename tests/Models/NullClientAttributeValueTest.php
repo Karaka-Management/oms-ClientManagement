09a1cba17c15d523c82f837d4759767688fc3852
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-21 22:16:06 +0200

    more tests and make classes final

---
+++ tests/Models/NullClientAttributeValueTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\ClientManagement\tests\Models;
+
+use Modules\ClientManagement\Models\NullClientAttributeValue;
+
+/**
+ * @internal
+ */
+final class NullClientAttributeValueTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\ClientManagement\Models\NullClientAttributeValue
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\ClientManagement\Models\ClientAttributeValue', new NullClientAttributeValue());
+    }
+
+    /**
+     * @covers Modules\ClientManagement\Models\NullClientAttributeValue
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullClientAttributeValue(2);
+        self::assertEquals(2, $null->getId());
+    }
+}
