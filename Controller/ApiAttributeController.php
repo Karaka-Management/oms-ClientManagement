ccea97378ddb417f6ebaa6081971d3e4e0221529
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-19 17:17:07 +0000

    fix value change log

--- Controller/ApiAttributeController.php
+++ Controller/ApiAttributeController.php
@@ -231,7 +231,9 @@ final class ApiAttributeController extends Controller
 
         $this->updateModel($request->header->account, $old, $new, ClientAttributeMapper::class, 'client_attribute', $request->getOrigin());
 
-        if ($new->value->getValue() !== $old->value->getValue()) {
+        if ($new->value->getValue() !== $old->value->getValue()
+            && $new->type->custom
+        ) {
             $this->updateModel($request->header->account, $old->value, $new->value, ClientAttributeValueMapper::class, 'attribute_value', $request->getOrigin());
         }
 
