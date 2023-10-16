eecd67bba2bb46c4d2bf8523f7601e8aa08e5852
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:39:38 +0000

    Add test cases for null models

--- tests/Models/NullClientTest.php
+++ tests/Models/NullClientTest.php
@@ -23,7 +23,7 @@ final class NullClientTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\ClientManagement\Models\NullClient
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullClientTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\ClientManagement\Models\NullClient
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
